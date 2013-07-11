9e096a4d98df237669757ec7e8404156d0cad879
Ubuntu <ubuntu@ip-10-164-47-253.ec2.internal>
2013-07-11 22:53:23 +0000

    Updated web.js to read index.html 'template'

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var templateFile = "index.html";
+  var templateRaw = fs.readFileSync(templateFile, {flag:'r', encoding:'utf-8'});
+  response.send(templateRaw);
 });
 
 var port = process.env.PORT || 5000;
